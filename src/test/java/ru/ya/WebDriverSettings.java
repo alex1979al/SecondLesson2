fd90ba381ad0c2a06342aebaffa20e682abce98e
Meshcheryakov Aleksey <alex_mesch@mail.ru>
2021-07-21 15:17:55 +0300

    Initial commit

---
+++ src/test/java/ru/ya/WebDriverSettings.java
@@ -0,0 +1,27 @@
+package ru.ya;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class WebDriverSettings {
+    WebDriver chromeDriver;
+
+    @BeforeEach
+    public void start(){
+        System.setProperty ("webdriver.chrome.driver",System.getenv("CHROME_DRIVER"));
+        chromeDriver=new ChromeDriver();
+        chromeDriver.manage().window().maximize();
+        chromeDriver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+    }
+
+    @AfterEach
+    public void close(){
+      chromeDriver.quit();
+
+    }
+}
